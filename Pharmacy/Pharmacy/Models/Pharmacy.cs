88f84d59cf2c1704936807aa228225cb8541d3ed
Nikolay Tsvetin <nikolaytsvetin@gmail.com>
2021-03-08 23:05:01 +0200

    Added models, setuped database etc...

---
+++ Pharmacy/Pharmacy/Models/Pharmacy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Models
+{
+    public class Pharmacy
+    {
+        public string City { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
--- Pharmacy/Pharmacy/Startup.cs
+++ Pharmacy/Pharmacy/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pharmacy.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +26,13 @@ namespace Pharmacy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PharmacyContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<Customer, IdentityRole>(options => {
+                options.Password.RequiredLength = 10;
+            }).AddEntityFrameworkStores<PharmacyContext>();
+
             services.AddControllersWithViews();
         }
 
