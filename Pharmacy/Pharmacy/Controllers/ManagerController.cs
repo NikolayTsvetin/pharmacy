aa9b7841ddb7ee1f8b795bb6d487b4ad2f3dc0fd
Nikolay Tsvetin <nikolaytsvetin@gmail.com>
2021-05-04 18:12:43 +0300

    Admin functionality to add/remove users from/to roles.

--- Pharmacy/Pharmacy/Controllers/ManagerController.cs
+++ Pharmacy/Pharmacy/Controllers/ManagerController.cs
@@ -136,6 +136,91 @@ namespace Pharmacy.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string id)
+        {
+            ViewBag.roleId = id;
+
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id {id} cannot be found.";
+
+                return View("NotFound");
+            }
+
+            var model = new List<UserRoleViewModel>();
+
+            foreach (var user in _userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel() { UserId = user.Id, UserName = user.UserName };
+                bool isUserInRole = await _userManager.IsInRoleAsync(user, role.Name);
+
+                if (isUserInRole)
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id {id} cannot be found.";
+
+                return View("NotFound");
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                UserRoleViewModel currentUser = model[i];
+                var user = await _userManager.FindByIdAsync(currentUser.UserId);
+                IdentityResult result = null;
+                var userIsInRole = await _userManager.IsInRoleAsync(user, role.Name);
+
+                if (currentUser.IsSelected && !userIsInRole)
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!currentUser.IsSelected && userIsInRole)
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < model.Count - 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return RedirectToAction("EditRole", new { Id = id });
+                    }
+                }
+
+            }
+
+            return RedirectToAction("EditRole", new { Id = id });
+        }
+
+        [HttpGet]
         public IActionResult AllUsers()
         {
             var users = _userManager.Users;
