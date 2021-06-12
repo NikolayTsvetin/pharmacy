1eec204612aab794457870f9e44c40e025b2d68d
Nikolay Tsvetin <nikolaytsvetin@gmail.com>
2021-06-12 17:17:51 +0300

    Display current location on embedded google maps - WIP.

--- Pharmacy/Pharmacy/Controllers/HomeController.cs
+++ Pharmacy/Pharmacy/Controllers/HomeController.cs
@@ -39,6 +39,12 @@ namespace Pharmacy.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Location()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
